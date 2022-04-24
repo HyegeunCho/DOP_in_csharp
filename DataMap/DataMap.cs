a3e8431c7ca6d25782f1cff7d5512d466e3eeb21
stzhyegeun <hyegeun.cho@sundaytoz.com>
2022-04-24 18:49:10 +0900

    [InitialCommit]

---
+++ DataMap/DataMap.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Runtime.ExceptionServices;
+
+namespace DataMap
+{
+    public class DataMap
+    {
+        private Hashtable _map;
+
+        public DataMap()
+        {
+            _map = new Hashtable();
+        }
+
+        public object GetData(params string[] inParam)
+        {
+            if (inParam.Length == 0) return _map;
+
+            object currentNode = _map;
+            for (int i = 0; i < inParam.Length; i++)
+            {
+                if ((currentNode is Hashtable) == false) return currentNode;
+                currentNode = (currentNode as Hashtable)[inParam[i]];
+            }
+
+            return currentNode;
+        }
+
+        public bool SetData(object inValue, params string[] inParam)
+        {
+            object currentNode = _map;
+            for (int i = 0; i < inParam.Length; i++)
+            {
+                if (currentNode == null)
+                {
+                    currentNode = new Hashtable();
+                }
+                else if (currentNode is Hashtable)
+                {
+                    var currentHashtable = currentNode as Hashtable;
+                    if (currentHashtable.ContainsKey(inParam[i]))
+                    {
+                        currentNode = currentHashtable[inParam[i]];
+                    }
+                    else
+                    {
+                        if (i == inParam.Length - 1)
+                        {
+                            currentHashtable.Add(inParam[i], inValue);    
+                        }
+                        else
+                        {
+                            currentHashtable.Add(inParam[i], new Hashtable());    
+                        }
+                        
+                        currentNode = currentHashtable[inParam[i]];
+                    }
+                }
+            }
+            return true;
+        }
+        
+        public static DataMap Create()
+        {
+            DataMap result = new DataMap();
+            return result;
+        }
+        
+        
+    }
+}
---
+++ DataMap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DataMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataMap map = DataMap.Create();
+            map.SetData(1, "root", "first");
+
+            var first = map.GetData("root", "first");
+            Console.WriteLine(first);
+
+            var second = map.GetData("root", "second");
+            Console.WriteLine(second);
+        }
+    }
+}
